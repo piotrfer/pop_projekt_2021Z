98df805eec858df25e500d7e4d612c005b4f8b82
unknown <mjgajda@icloud.com>
2021-11-02 20:05:03 +0100

    fetch added

--- Frontend/src/app/presenters/forestry-presenter/forestry-presenter.ts
+++ Frontend/src/app/presenters/forestry-presenter/forestry-presenter.ts
@@ -1,11 +1,12 @@
 import { ForestryShower } from 'src/app/interfaces/forestry-shower';
 import { ForestryEventHandler } from "src/app/interfaces/forestry-event-handler";
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { ForestryDto } from 'src/app/data-types/forestry-dto';
 
 export class ForestryPresenter implements ForestryEventHandler {
     private forestryView: ForestryShower|undefined;
+    baseURL: string = "http://127.0.0.1:5000/"
 
     constructor(private http: HttpClient) { }
 
@@ -24,5 +25,13 @@ export class ForestryPresenter implements ForestryEventHandler {
     // }
 
      saveForestryClicked(forestry: ForestryDto): void{
+       const httpOptions = {
+         headers: new HttpHeaders({
+           'Content-Type':  'application/json'
+         })
+       };
+       const body=forestry;
+       console.log(body)
+       this.http.post(this.baseURL + 'forestry', body, httpOptions).subscribe(forestry => console.log(forestry));
     }
 }
